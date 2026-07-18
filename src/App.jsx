4b7c836038a073398dc8a5dbe38bcede7e60509c
Matb <matb.coder@gmail.com>
2026-07-18 18:07:02 +0200

    Routes definition and enabled routing, in  App.jsx and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useEffect } from "react";
-import api from "./api/axios";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Students from "./pages/Students";
+import AddStudent from "./pages/AddStudent";
+import EditStudent from "./pages/EditStudent";
 
 function App() {
-
-
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/students" element={<Students />} />
+            <Route path="/students/add" element={<AddStudent />} />
+            <Route path="/students/edit/:id" element={<EditStudent />} />
+        </Routes>
+    );
 }
 
 export default App;
---
+++ src/pages/EditStudent.jsx
@@ -0,0 +1,5 @@
+function EditStudent(){
+    return <h1>Edit Student page</h1>
+}   
+
+export default EditStudent;
