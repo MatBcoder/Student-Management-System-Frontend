9b2e978a9ee2cba77868ba82cf4a0e7a9e30c9c2
Matb <matb.coder@gmail.com>
2026-07-18 16:41:44 +0200

    Application structure, Pages and Components files

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,6 @@ import api from "./api/axios";
 
 function App() {
 
-    useEffect(() => {
-
-        api.get("/students")
-            .then(response => {
-                console.log("Connection successful");
-                console.log(response.data);
-            })
-            .catch(error => {
-                console.error("Connection failed");
-                console.error(error);
-            });
-
-    }, []);
-
-    return (
-        <h1>Student Management System</h1>
-    );
 
 }
 
