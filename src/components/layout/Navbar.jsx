bb30c68bcd0e021bac9fa6c3f76b8bd7f34cf0d0
Matb <matb.coder@gmail.com>
2026-07-24 16:21:17 +0200

    Bootstrap and the Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,18 @@
+import {NavLink} from "react-router-dom";
+
 function Navbar() {
     return (
-        <nav>
-            <h2>Student Management System</h2>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div classname= "container">
+                <NavLink className="navbar-brand" to="/">Student Management</NavLink>
+                <div className= "navbar-nav">
+                    <NavLink className = "nav-link" to="/">Home</NavLink>
+                    <NavLink className = "nav-link" to="/students">Students</NavLink>
+                    <NavLink className = "nav-link" to="/students/add">Add Student</NavLink>
+                    
+                </div>
+
+            </div>
         </nav>
     );
 }
