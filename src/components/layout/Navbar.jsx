e94328c93c3ae3731fd6a0ca0904a72d3211e472
Matb <matb.coder@gmail.com>
2026-07-24 17:06:53 +0200

    Navigation

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,13 +3,12 @@ import {NavLink} from "react-router-dom";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div classname= "container">
-                <NavLink className="navbar-brand" to="/">Student Management</NavLink>
+          <div classname= "container">
+                <NavLink className="navbar-brand" to="/">SMS Portal</NavLink>
                 <div className= "navbar-nav">
-                    <NavLink className = "nav-link" to="/">Home</NavLink>
-                    <NavLink className = "nav-link" to="/students">Students</NavLink>
-                    <NavLink className = "nav-link" to="/students/add">Add Student</NavLink>
-                    
+                    <NavLink to="/" className={({ isActive }) =>isActive ? "nav-link active fw-bold" : "nav-link"}>Home</NavLink>
+                    <NavLink to="/students" end className={({ isActive }) =>isActive ? "nav-link active fw-bold" : "nav-link"}>Students</NavLink>
+                    <NavLink to="/students/add" className={({ isActive }) =>isActive ? "nav-link active fw-bold" : "nav-link"}>Add Student</NavLink>
                 </div>
 
             </div>
@@ -18,3 +17,9 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+{/* <NavLink className = "nav-link" to="/">Home</NavLink>
+    <NavLink className = "nav-link" to="/students">Students</NavLink>
+    <NavLink className = "nav-link" to="/students/add">Add Student</NavLink>*/}
+                
